81544305da9c600daf318a5ed063596da645fb85
Polaris <hsd18624082451@163.com>
2025-03-22 19:51:36 +0800

    debug

--- create/assets/js/create.js
+++ create/assets/js/create.js
@@ -120,6 +120,7 @@ async function submitSurvey() {
     if (hasError) return;
     try {
         const token = localStorage.getItem('authToken');
+        localStorage.setItem("json-survey", JSON.stringify(surveyData));
         const response = await fetch('https://api.clovenova.cn/api/create-survey', {
             method: 'POST',
             headers: {
